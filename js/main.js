39bf3f2c57acc51380414ed7a16e664d492d0f5f
Gabriel Mojica <niko.mojica@live.com>
2016-07-30 20:00:37 +1000

    Render markdown headings

--- js/main.js
+++ js/main.js
@@ -1,13 +1,29 @@
 $(function() {
-    var editorView = $('#editor-view');
-    var markdownView = $('#markdown-view');
+    var $editorView = $('#editor-view');
+    var $markdownView = $('#markdown-view');
 
     function renderMarkdown() {
-        var value = this.value;
+        var lines = this.value.split('\n');
 
+        $markdownView.empty();
 
+        lines.forEach(function(line) {
+            var $p = $('<p></p>');
+
+            var heading = '######';
+            for (var i = 6; i >= 1; i--) {
+                if (line.startsWith(heading)) {
+                    line = line.substring(i);
+                    $p.addClass('h' + i);
+                    break;
+                }
+                heading = heading.substring(1);
+            }
+
+            $p.text(line);
+            $markdownView.append($p);
+        });
     }
 
-    editorView.on('input', renderMarkdown);
-
+    $editorView.on('input', renderMarkdown);
 });
