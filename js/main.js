f973e07508bc139e077de73fae646aea89362c8e
Gabriel Mojica <niko.mojica@live.com>
2016-07-30 19:04:18 +1000

    Get editor text value on input

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
 $(function() {
+    var editorView = $('#editor-view');
+    var markdownView = $('#markdown-view');
 
+    function renderMarkdown() {
+        var value = this.value;
+
+
+    }
+
+    editorView.on('input', renderMarkdown);
 
 });
